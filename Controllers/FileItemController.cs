c8a39323d3e27e695d6b142c00899f83eefaca72
Martin Flasar <christian.flasar@gmail.com>
2024-03-20 03:07:33 +0100

    Add project files.

---
+++ Controllers/FileItemController.cs
@@ -0,0 +1,26 @@
+﻿using Filumder_API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Filumder_API.Controllers
+{
+    [ApiController]
+    [Route("api/File")]
+    public class FileItemController : Controller
+    {
+        private readonly ILogger<FileItemController> _logger;
+
+        public FileItemController(ILogger<FileItemController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("DownloadFile")]
+        public IActionResult DownloadFile()
+        {
+            // Replace "external_file_url" with the actual URL of the file
+            var externalFileUrl = "https://example.com/path/to/file.ext";
+            return Ok(new { Url = externalFileUrl, _fileItem = new FileItem("TestName", "TestDescription", "TestURL") });
+        }
+    }
+}
---
+++ Entities/FileItem.cs
@@ -0,0 +1,16 @@
+﻿namespace Filumder_API.Entities
+{
+    public class FileItem
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+
+        public FileItem(string name, string description, string url)
+        {
+            Name = name;
+            Description = description;
+            Url = url;
+        }
+    }
+}
